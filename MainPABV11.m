efc6a6ec600583a25a83d91b514be56f8e697297
Rui Li <liruibdwdm@yeah.net>
2018-03-21 05:18:29 -0400

    Update MainPABV11.m

--- MainPABV11.m
+++ MainPABV11.m
@@ -3,6 +3,17 @@
 % Author: Ricky (Rui Li) at Tsinghua & Harvard university
 % E-mail: eeairicky@gmail.com eeairicky@seas.harvard.edu
 % Version: 8.1 2017/12/03 2018/03/18
+% Copyright (C) 2017 EH-PAB Project Rui Li
+
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
 %========================================================================
 clc; close all; clear all;
 format short;
